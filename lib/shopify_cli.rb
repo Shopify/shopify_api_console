145df12608ac22221d4f50dd44461b07b6b2f789
Paulo Margarido <paulo.margarido@shopify.com>
2020-06-22 14:06:13 -0400

    Remove the code from the old CLI

--- lib/shopify_cli.rb
+++ lib/shopify_cli.rb
@@ -1,4 +1,7 @@
 module ShopifyCLI
-  VERSION = "1.0.4"
-  warn "[DEPRECATION] This gem has been renamed to shopify_api_console. Please switch to shopify_api_console v2+ as soon as possible."
+  warn <<~MESSAGE
+    [DEPRECATION] The shopify_cli gem is no longer supported. Please switch to:
+      - shopify_api_console v2+ for the Shopify API console
+      - shopify-cli for the Shopify CLI
+  MESSAGE
 end
